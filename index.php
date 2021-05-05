5093971ff5549497039d42b2c9ce6dad18562b95
khang <banhbeovodung01@gmail.com>
2021-05-05 13:30:52 +0700

    them front-end

--- index.php
+++ index.php
@@ -4,22 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tra Luan Van</title>
 </head>
 <body>
-<h1>hello</h1>
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "123";
-$dbname = "myDB";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-$conn->close();
-
-?> 
+    
 </body>
 </html>
