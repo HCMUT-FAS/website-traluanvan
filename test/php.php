c263254dc9c00c5db38c3c1d0d2914accad0d064
khang <banhbeovodung01@gmail.com>
2021-06-27 13:42:38 +0700

    fix %20 %% %25 search

--- test/php.php
+++
@@ -1,8 +0,0 @@
-<?php    
-$s = '     ';
-// $s = str_replace(" ", "", $s);
-if(empty(str_replace(" ", "", $s))){
-    echo '1';
-}else {
-    echo '0';
-}
--- view.php
+++ view.php
@@ -5,26 +5,32 @@
 
 include "include/searchbox-view.php";
 include "include/displayData-view.php";
-
 // Chua biet cach hoat dong cua isset()
 // Khi Search query khong co trong file view.php
 if (isset($_GET['search-submit'])) {
     require "Database/conn.php";
-    // $search = $_GET['s'];
-    if (empty(str_replace(" ", "", $_GET['s']))) {
+    // Khong biet sua may cai loi nay nhu nao
+    // khi bo cac dong thay the nay di thi khi tim kiem bang %20 hay %25 hay %%% thi ra het tat ca luan van
+    $s = $_GET['s'];
+    $s = urldecode($s);
+    $s = str_replace('%20', '', $s);
+    $s = str_replace('%25', '', $s);
+    $s = str_replace('%', '', $s);
+    $s = str_replace(' ', '', $s);
+    if (empty($s)) {
         header("Location: index?error=emptysearch");
         exit();
     } else {
-        $search = "%" . $_GET['s'] . "%";
+        $s = $_GET['s'];
+        $search = "%" . $s . "%";
         $stmt = $conn->prepare("SELECT * FROM $table WHERE LV_Ten LIKE ? OR GV1_Ten LIKE ? OR GV2_Ten LIKE ? OR LV_Ma LIKE ? OR SV1_Ten LIKE ? OR SV2_Ten LIKE ? OR MSSV1 LIKE ? OR MSSV2 LIKE ?;");
         $stmt->bind_param('ssssssss', $search, $search, $search, $search, $search, $search, $search, $search);
         $stmt->execute();
         $result = $stmt->get_result();
         printf("Có %u giá trị tìm kiếm! <br>", $result->num_rows);
-        // echo $result->num_rows . "<br>";
-        $s = $_GET['s'];
-        $header = 'Location: view?&q=' . $s . '&error=notfound2';
+
         if (strval($result->num_rows) == "0") {
+            $header = 'Location: view?&q=' . $_GET['s'] . '&error=notfound2';
             header("$header");
             exit();
         }
@@ -44,7 +50,7 @@ if (isset($_GET['search-submit'])) {
     }
 } else {
     $s = $_GET['q'];
-    echo '<p>Your Search - ' . $s . '- did not match any documents.</p>
+    echo '<p>Your Search - ' . $s . ' - did not match any documents.</p>
     <p>Suggestions:</p>
     <ul>
         <li>Tên Giảng Viên: Mai Hữu Xuân, Lê Quốc Khải, Trần Minh Thái,...</li>
