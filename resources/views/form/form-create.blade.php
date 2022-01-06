315a564198f7fffe52c2b14985500ce94b902977
Khang <banhbeovodung01@gmail.com>
2022-01-06 15:27:37 +0700

    feat(form-create) not allow guest open form

--- resources/views/form/form-create.blade.php
+++ resources/views/form/form-create.blade.php
@@ -4,48 +4,55 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h2 class="modal-title" id="exampleModalLabel">Thông tin của bạn</h2>
-            </div>
-            <div class="modal-body">
-                <form action="{{ route('thesis-form') }}" method="POST">
-                    @csrf
-                    <div class="card-body">
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Tên Sinh Viên</label>
-                            <input type="text" value="{{ Auth::user()->name }}" disabled="disabled" class="form-control" />
+@guest
+
+@else
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h2 class="modal-title" id="exampleModalLabel">Thông tin của bạn</h2>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('thesis-form') }}" method="POST">
+                        @csrf
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Tên Sinh Viên</label>
+                                <input type="text" value="{{ Auth::user()->name }}" disabled="disabled"
+                                    class="form-control" />
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email Sinh Viên</label>
+                                <input type="text" value="{{ Auth::user()->email }}" disabled="disabled"
+                                    class="form-control" />
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Tên luận văn</label>
+                                <input type="text" value="{{ $thesis->nameVN }}" disabled="disabled"
+                                    class="form-control" />
+                                <input type="hidden" name="thesis_id" value="{{ $thesis->id }}" class="form-control" />
+                                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}"
+                                    class="form-control" />
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Ngày dự kiến đến mượn</label>
+                                @error('date')
+                                    <span class="form-group" role="alert">
+                                        <strong style="color: red">{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                                <input type="date" name='expected_date' class="form-control" value="{{ old('date') }}">
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Email Sinh Viên</label>
-                            <input type="text" value="{{ Auth::user()->email }}" disabled="disabled" class="form-control" />
+                        <!-- /.card-body -->
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Tên luận văn</label>
-                            <input type="text" value="{{ $thesis->nameVN }}" disabled="disabled" class="form-control" />
-                            <input type="hidden" name="thesis_id" value="{{ $thesis->id }}" class="form-control" />
-                            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}"
-                                class="form-control" />
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Ngày dự kiến đến mượn</label>
-                            @error('date')
-                                <span class="form-group" role="alert">
-                                    <strong style="color: red">{{ $message }}</strong>
-                                </span>
-                            @enderror
-                            <input type="date" name='expected_date' class="form-control" value="{{ old('date') }}">
-                        </div>
-                    </div>
-                    <!-- /.card-body -->
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+@endguest
