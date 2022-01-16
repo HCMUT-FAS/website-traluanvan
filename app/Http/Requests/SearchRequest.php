a899315b1c263cdf818c999414462630b1322767
Khang <banhbeovodung01@gmail.com>
2022-01-16 07:01:08 +0700

    fix(Search Request) minimum for search is 2 letters

--- app/Http/Requests/SearchRequest.php
+++ app/Http/Requests/SearchRequest.php
@@ -24,7 +24,7 @@ class SearchRequest extends FormRequest
     public function rules()
     {
         return [
-            'search' => ['required', 'min:5']
+            'search' => ['required', 'min:2']
         ];
     }
 }
