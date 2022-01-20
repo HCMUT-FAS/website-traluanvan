6fa7dad8bc0586ab77e763a610febc334527d74b
Khang <banhbeovodung01@gmail.com>
2022-01-21 06:36:08 +0700

    Style(errors) add 500, 404 pages

---
+++ resources/views/error/404.blade.php
@@ -0,0 +1,21 @@
+<div class="content-wrapper" style="min-height: 1604.44px;">
+    <!-- Main content -->
+    <section class="content">
+        <div class="error-page">
+            <h2 class="headline text-warning"> 404</h2>
+
+            <div class="error-content">
+                <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Page not found.</h3>
+
+                <p>
+                    We could not find the page you were looking for.
+                    Meanwhile, you may <a href="{{ route('') }}">return to dashboard</a> or try using the search
+                    form.
+                </p>
+            </div>
+            <!-- /.error-content -->
+        </div>
+        <!-- /.error-page -->
+    </section>
+    <!-- /.content -->
+</div>
---
+++ resources/views/error/500.blade.php
@@ -0,0 +1,20 @@
+<div class="content-wrapper" style="min-height: 1604.44px;">
+    <!-- Main content -->
+    <section class="content">
+        <div class="error-page">
+            <h2 class="headline text-danger">500</h2>
+
+            <div class="error-content">
+                <h3><i class="fas fa-exclamation-triangle text-danger"></i> Oops! Something went wrong.</h3>
+
+                <p>
+                    We will work on fixing that right away.
+                    Meanwhile, you may <a href="../../index.html">return to dashboard</a> or try using the search form.
+                </p>
+            </div>
+        </div>
+        <!-- /.error-page -->
+
+    </section>
+    <!-- /.content -->
+</div>
