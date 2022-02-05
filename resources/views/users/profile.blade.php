dde75e8497aac304b873a360e588e63a3d406672
Khang <banhbeovodung01@gmail.com>
2022-02-05 14:12:28 +0700

    Style(ProfileController) display error

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -26,15 +26,24 @@
                                             <div class="col-sm-10">
                                                 <input name="name" class="form-control" id="inputName"
                                                     style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;"
-                                                    value="{{ $user->name }}">
+                                                    value="{{ $user->name }}" required>
                                                 <input type="hidden" name="id" value="{{ $user->id }}">
+                                                @error('name')
+                                                    <strong class='text-danger'>{{ $message }}</strong>
+                                                @enderror
+                                                @error('id')
+                                                    <strong class='text-danger'>{{ $message }}</strong>
+                                                @enderror
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
                                             <div class="col-sm-10">
-                                                <input class="form-control" id="inputExperience" name="email"
+                                                <input type="email" class="form-control" id="inputExperience" name="email"
                                                     value="{{ $user->email }}"></input>
+                                                @error('email')
+                                                    <strong class='text-danger'>{{ $message }}</strong>
+                                                @enderror
                                             </div>
                                         </div>
                                         <div class="form-group row">
