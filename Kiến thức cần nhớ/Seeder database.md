012081d33381949cc9991159f1dd93fbf7d7fe4e
Khang <banhbeovodung01@gmail.com>
2022-03-29 13:00:18 +0700

    docs update

---
+++ Kiến thức cần nhớ/Seeder database.md
@@ -0,0 +1,8 @@
+Tạo seed
+`php artisan make:seeder <ten-seeder>`
+`php artisan make:seeder UserSeeder`
+
+
+Chạy seed
+`php artisan db:seed --class=<chay-seeder>`
+`php artisan db:seed --class=UserHasTheses`
--- Kiến thức cần nhớ/initial data.sql
+++ Kiến thức cần nhớ/initial data.sql
@@ -7,11 +7,6 @@
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -40,24 +35,6 @@ INSERT INTO `faculties` (`id`, `name`) VALUES
 (12, 'Trung tâm Đào tạo Bảo dưỡng Công nghiệp');
 
 --
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_100000_create_password_resets_table', 1),
-(2, '2019_08_19_000001_create_failed_jobs_table', 1),
-(3, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(4, '2022_01_03_000000_create_faculties_table', 1),
-(5, '2022_01_03_000000_create_thesis_roles_table', 1),
-(6, '2022_01_03_000000_create_thesis_statuses_table', 1),
-(7, '2022_01_03_000000_create_user_roles_table', 1),
-(8, '2022_01_03_000001_create_users_table', 1),
-(9, '2022_01_03_010121_create_theses_table', 1),
-(10, '2022_01_20_051653_create_jobs_table', 1),
-(11, '2022_03_27_083805_create_user_issues_theses_table', 1),
-(12, '2022_03_27_083959_create_user_has_theses_table', 1);
-
---
 -- Dumping data for table `thesis_roles`
 --
 
@@ -83,7 +60,7 @@ INSERT INTO `user_roles` (`id`, `name`) VALUES
 (1, 'Student'),
 (2, 'Librarian'),
 (3, 'Professor');
-COMMIT;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
