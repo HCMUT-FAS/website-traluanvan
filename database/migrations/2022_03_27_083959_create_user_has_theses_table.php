d23abf5d64ee5a7aa448f1e36bec0b5b8e742957
Khang <banhbeovodung01@gmail.com>
2022-03-29 12:58:04 +0700

    refactor (database) remove unused foreign key
    remove column: user_faculty_id, user_role_id, thesis_status_id, thesis_role_id

--- database/migrations/2022_03_27_083959_create_user_has_theses_table.php
+++ database/migrations/2022_03_27_083959_create_user_has_theses_table.php
@@ -14,24 +14,13 @@ class CreateUserHasThesesTable extends Migration
     public function up()
     {
         Schema::create('user_has_theses', function (Blueprint $table) {
+            $table->id();
             // user_id (FK)
             $table->string('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            // user_faculty_id (FK)
-            $table->unsignedBigInteger('user_faculty_id');
-            $table->foreign('user_faculty_id')->references('user_faculty_id')->on('users');
-            // user_role_id (FK)
-            $table->unsignedBigInteger('user_role_id');
-            $table->foreign('user_role_id')->references('user_role_id')->on('users');
             // thesis_id (FK)
             $table->string('thesis_id');
             $table->foreign('thesis_id')->references('id')->on('theses');
-            // thesis_status_id (FK)
-            $table->unsignedBigInteger('thesis_status_id');
-            $table->foreign('thesis_status_id')->references('thesis_status_id')->on('theses');
-            // thesis_role_id (FK)
-            $table->unsignedBigInteger('thesis_role_id');
-            $table->foreign('thesis_role_id')->references('thesis_role_id')->on('theses');
         });
     }
 
