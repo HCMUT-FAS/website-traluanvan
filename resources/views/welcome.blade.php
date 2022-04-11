886f4021b328c851c1f9096708c5cad205c59abb
Khang <banhbeovodung01@gmail.com>
2022-04-11 21:48:14 +0700

    Refactor (change file name)
    layouts/app.blade.php -> layouts/master.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,33 +1,32 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('search')
-        <div class="content">
-            <section class="content">
-                <div class="container-fluid">
-                    <h3 class="text-center display-4">Tìm Kiếm Luận Văn Theo Chủ Đề</h3>
-                    <div class="row">
-                        <div class="col-md-8 offset-md-2">
-                            <form action="{{ route('thesis-search') }}" method="get">
-                                @csrf
-                                <div class="input-group">
-                                    <input type="search" name="search" class="form-control form-control-lg"
-                                        placeholder="Type your keywords here" required>
-                                    <div class="input-group-append">
-                                        <button type="submit" class="btn btn-lg btn-default">
-                                            <i class="fa fa-search"></i>
-                                        </button>
-                                    </div>
+    <div class="content">
+        <section class="content">
+            <div class="container-fluid">
+                <h3 class="text-center display-4">Tìm Kiếm Luận Văn Theo Chủ Đề</h3>
+                <div class="row">
+                    <div class="col-md-8 offset-md-2">
+                        <form action="{{ route('thesis.search') }}" method="get">
+                            <div class="input-group">
+                                <input type="search" name="search" class="form-control form-control-lg"
+                                    placeholder="Type your keywords here" required>
+                                <div class="input-group-append">
+                                    <button type="submit" class="btn btn-lg btn-default">
+                                        <i class="fa fa-search"></i>
+                                    </button>
                                 </div>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </section>
-            @error('search')
-                <span class=" col-md-8 offset-md-2" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
+            </div>
+        </section>
+        @error('search')
+            <span class=" col-md-8 offset-md-2" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
     </div>
 @endsection
